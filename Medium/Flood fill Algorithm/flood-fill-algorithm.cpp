fdd9b48a43a5f686959b6aa3c8cb78fdb1af191f
shriyal22 <88363808+shriyal22@users.noreply.github.com>
2024-02-14 18:30:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    void dfs(int row, int col, vector<vector<int>>&vis,
+     vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
+     int iniColor) {
+        // color with new color
+        vis[row][col] = newColor; 
+        int n = image.size();
+        int m = image[0].size(); 
+        // there are exactly 4 neighbours
+        for(int i = 0;i<4;i++) {
+            int nrow = row + delRow[i]; 
+            int ncol = col + delCol[i]; 
+            // check for valid coordinate 
+            // then check for same initial color and unvisited pixel
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
+            image[nrow][ncol] == iniColor && vis[nrow][ncol] != newColor) {
+                dfs(nrow, ncol, vis, image, newColor, delRow, delCol, iniColor); 
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        // Code here 
+        int iColor = image[sr][sc];
+        
+        vector<vector<int>> vis = image;
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+        dfs(sr, sc, vis, image, newColor, delRow, delCol, iColor);
+        
+        return vis;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>image(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cin >> image[i][j];
+		}
+		int sr, sc, newColor;
+		cin >> sr >> sc >> newColor;
+		Solution obj;
+		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+		for(auto i: ans){
+			for(auto j: i)
+				cout << j << " ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
